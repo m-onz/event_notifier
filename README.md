71e1c5206880c54e9272d47bf75ac458034d651d
m-onz <monz@startmail.com>
2023-10-02 16:47:55 +0100

    ...

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # event_notifier
-a simple cli tool that notifies me about upcoming events
+
+a simple cli tool that notifies me about upcoming events.
+
+## the problem I'm solving
+
+I want to get notified about future one off events
+and recurring events like friends and families birthdays.
+
+I want a simple cli tool and JSON file to represent these
+events. I will simply modify the JSON file directly to add new
+events.
+
+When I log into my computer this cli tool will be launched via
+.bashrc creating notifications with `node-notifier`.
+
+This is my personal tool that is exactly as complicated as it
+needs to be. It does not use cron or have a GUI so its
+extremely minimal as a solution but that's what I want.
+
+## how it works.
+
+* install via github
+* cd ./event_notifier && npm i && sudo npm link
+* edit your `events.json` with your actual data
+* update your ./.bashrc with `(sleep 180 && event_notifier) &`
+
+upon logging in you should see notifications for:
+
+* events that are happening today
+* upcoming events in exactly 7 days
+* all upcoming events in the next 30 days.
+
+if you don't actually log into your computer you will never get notified.
+this is designed to work and tested on debian based linux systems (linux mint).
+should be easy to get it to work on other platforms.
---
+++ bin.js
@@ -0,0 +1,68 @@
+
+var fs = require('fs')
+var moment = require('moment')
+var notifier = require('node-notifier')
+var EVENTS_FILE = 'events.json'
+var events = []
+
+if (fs.existsSync(EVENTS_FILE)) {
+  events = JSON.parse(fs.readFileSync(EVENTS_FILE, 'utf-8'))
+}
+
+var checkNotifications = () => {
+  console.log('checking for events notifications:')
+  events.forEach(event => {
+    switch (event.frequency) {
+      case 'yearly':
+        checkOneWeekBefore(event, true)
+        checkOnTheDay(event, true)
+        break;
+      default:
+        checkOneWeekBefore(event)
+        checkOnTheDay(event)
+    }
+  })
+}
+
+var checkOneWeekBefore = (event, ignoreYear = false) => {
+  var today = moment();
+  var eventDate = ignoreYear ? moment(event.date).year(today.year()) : moment(event.date)
+  var oneWeekBefore = eventDate.clone().subtract(7, 'days')
+  if (today.isSame(oneWeekBefore, 'day')) {
+    notify(`Reminder: ${event.name} ${event.description} is one week away!`)
+  }
+}
+
+var checkOnTheDay = (event, ignoreYear = false) => {
+  var today = moment();
+  var eventDate = ignoreYear ? moment(event.date).year(today.year()) : moment(event.date)
+  if (today.isSame(eventDate, 'day')) {
+    notify(`Reminder: Today is ${event.name} ${event.description}`)
+  }
+}
+
+var upcomingEventsInNext30Days = () => {
+  console.log('Events in the next 30 days:')
+  events.forEach(event => {
+    var today = moment()
+    var eventDate = event.frequency === 'yearly' ? moment(event.date).year(today.year()) : moment(event.date)
+    if (eventDate.isAfter(today) && eventDate.diff(today, 'days') <= 30) {
+      notify(`${event.name}'s event is in ${eventDate.diff(today, 'days')} days.`)
+    }
+  })
+}
+
+checkNotifications()
+upcomingEventsInNext30Days()
+
+function notify (message) {
+  console.log(message)
+  notifier.notify(
+    {
+      title: 'event_notifier',
+      message: message,
+      sound: true,
+      wait: true
+    }
+  )
+}
